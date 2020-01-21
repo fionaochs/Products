b3e4cf106ccc9441025fa12ff39d1b33a843adad
Fiona Ochs <fiona.oaks@gmail.com>
2020-01-21 11:06:48 -0800

    bakedgoods added

---
+++ bakedGoods.js
@@ -0,0 +1,65 @@
+const muffin = {
+    id: 'muffin',
+    name: 'blueberry muffin',
+    img: 'muffin.png',
+    description: 'delicious breakfast snack, the top is the best part',
+    category: 'breakfast',
+    price: 4.00,
+    cost: 1.50
+}
+const croissant = {
+    id: 'croissant',
+    name: 'croissant',
+    img: 'croissant.png',
+    description: 'flakey and buttery, loved by the french',
+    category: 'breakfast',
+    price: 3.00,
+    cost: 1.50
+};
+const scone = {
+    id: 'scone',
+    name: 'scone',
+    img: 'scone.png',
+    description: 'dense breakfast of the English',
+    category: 'breakfast',
+    price: 4.00,
+    cost: 2.00
+};
+const pie = {
+    id: 'pie',
+    name: 'apple pie',
+    img: 'apple.png',
+    description: 'american classic',
+    category: 'dessert',
+    price: 14.00,
+    cost: 6.00
+};
+const cheesecake = {
+    id: 'cheesecake',
+    name: 'cheesecake',
+    img: 'cheesecake.png'
+    description: 'entire factories are devoted to these'
+    category: 'dessert',
+    price: 16.00,
+    cost: 8.00
+};
+const cupcake = {
+    id: 'cupcake',
+    name: 'cupcake',
+    img: 'cupcake.png'
+    description: 'sad little cakes',
+    category: 'dessert',
+    price: 4.00,
+    cost: 2.00
+};
+
+
+const bakedGoods = [
+    muffin,
+    croissant,
+    scone,
+    pie,
+    cheesecake,
+    cupcake
+];
+export default bakedGoods;
---
+++ products.js
@@ -0,0 +1,10 @@
+import bakedGoods from './bakedGoods.js';
+import renderBakedGoods from './render-bakedGoods.js';
+
+const list = document.getElementById('bakedGoods');
+
+for (let i = 0; i < bakedGoods.length; i++){
+    const bakedGoods = bakedGoods[i];
+    const dom = renderBakedGoods(bakedGoods);
+    list.appendChild(dom);
+}
