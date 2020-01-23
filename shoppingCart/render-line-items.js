52defe2910a8e6d3be5f72b01e9aad6f557b1f90
Fiona Ochs <fiona.oaks@gmail.com>
2020-01-23 09:57:56 -0800

    render function

--- shoppingCart/render-line-items.js
+++ shoppingCart/render-line-items.js
@@ -1,3 +1,27 @@
-import cart from "../data/cart.js";
-import bakedGoods from "../data/bakedGoods.js";
+function renderBakedGoods(lineItem, bakedGoods){
+    const tr = document.createElement('tr');
 
+    const nameCell = document.createElement('td');
+    nameCell.className = 'align left';
+    nameCell.textContent = bakedGoods.name;
+    tr.appendChild(nameCell);
+
+    const quantityCell = document.createElement('td');
+    quantityCell.textContent = lineItem.quantity;
+    tr.appendChild(quantityCell);
+
+    const priceCell = document.createElement('td');
+    nameCell.className = 'line-item-total';
+    priceCell.textContent = lineItem.price;
+    tr.appendChild(priceCell);
+
+    const totalCell = document.createElement('td');
+    const total = lineItem.quantity * bakedGoods.price;
+    totalCell.textContent = total;
+    tr.appendChild(totalCell);
+
+    return tr;
+
+}
+
+export default renderBakedGoods;
---
+++ shoppingCart/shoppingCart.js
@@ -0,0 +1,6 @@
+import renderBakedGoods from './render-line-items.js';
+import cart from '../data/cart.js';
+import bakedGoods from '../data/bakedGoods.js';
+import { findById, calcOrderTotal } from '../common/utils.js';
+
+
